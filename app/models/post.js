044f675047bfd53afbf5d7dd93c0f7173bc3623e
Alan Denison and Terri Bradley <alan+terri@>
2016-08-17 13:52:36 -0700

    added model route so individual model info can be displayed on new page

--- app/models/post.js
+++ app/models/post.js
@@ -1,5 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  "image": DS.attr(),
+  "title": DS.attr(),
+  "author": DS.attr(),
+  "date": DS.attr(),
+  "category": DS.attr(),
+  "story": DS.attr()
 
 });
