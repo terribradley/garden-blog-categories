ed3164c2e7b0de6e8b4464ae1c907c9fa873c820
Alan Denison and Terri Bradley <alan@>
2016-08-18 13:54:45 -0700

    add crud functionality for comments

--- app/components/comment-tile.js
+++ app/components/comment-tile.js
@@ -1,11 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    delete(comment) {
-      if(confirm("Are you sure?")) {
-        this.sendAction('destroyComment', comment);
-      }
-    }
-}
+
 });
--- app/routes/comment.js
+++ app/routes/comment.js
@@ -4,10 +4,10 @@ export default Ember.Route.extend({
   model(params) {
   return this.store.findRecord('comment', params.comment_id);
 },
-actions: {
-  destroyComment(comment) {
-    comment.destroyComment();
-    this.transitionTo('index');
+  actions: {
+    destroyComment(comment) {
+      comment.destroyRecord();
+      this.transitionTo('post', ((comment.post).post_id));
+    }
   }
-}
 });
