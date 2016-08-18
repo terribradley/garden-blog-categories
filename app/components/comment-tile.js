ee584d7c035c922e69254a245791614a3fc2849c
Alan Denison and Terri Bradley <alan+terri@>
2016-08-18 11:56:25 -0700

    work on delete comment function

--- app/components/comment-tile.js
+++ app/components/comment-tile.js
@@ -2,10 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    destroyComment(comment) {
+    delete(comment) {
       if(confirm("Are you sure?")) {
         this.sendAction('destroyComment', comment);
       }
     }
-  }
+}
 });
--- app/routes/comment.js
+++ app/routes/comment.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
   return this.store.findRecord('comment', params.comment_id);
+},
+actions: {
+  destroyComment(comment) {
+    comment.destroyComment();
+    this.transitionTo('index');
+  }
 }
 });
